a21e2581d5c27baa6b7f7788d8d29d2dc3c457de
keanna <keannamalien@gmail.com>
2024-09-09 22:47:45 -0700

    Added a condition to show full content on single pages only - content.php template part

--- template-parts/content.php
+++ template-parts/content.php
@@ -33,20 +33,27 @@
 
 	<div class="entry-content">
 		<?php
-		the_content(
-			sprintf(
-				wp_kses(
-					/* translators: %s: Name of current post. Only visible to screen readers */
-					__( 'Continue reading<span class="screen-reader-text"> "%s"</span>', 'school-theme' ),
-					array(
-						'span' => array(
-							'class' => array(),
-						),
-					)
-				),
-				wp_kses_post( get_the_title() )
-			)
-		);
+		// the_content(
+		// 	sprintf(
+		// 		wp_kses(
+		// 			/* translators: %s: Name of current post. Only visible to screen readers */
+		// 			__( 'Continue reading<span class="screen-reader-text"> "%s"</span>', 'school-theme' ),
+		// 			array(
+		// 				'span' => array(
+		// 					'class' => array(),
+		// 				),
+		// 			)
+		// 		),
+		// 		wp_kses_post( get_the_title() )
+		// 	)
+		// );
+		
+		// Display full content if the page is a single page, display excerpt for blog posts/archive
+		if(is_single()) {
+			the_content();
+		} else {
+			the_excerpt();
+		}
 
 		wp_link_pages(
 			array(
